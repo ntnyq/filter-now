f80e302b205388507caf857b680bf99f7d9576aa
ntnyq <ntnyq13@gmail.com>
2024-07-16 03:51:06 +0800

    fix: remove content_script

--- entrypoints/content/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <main
-    class="fixed right-0 bottom-[100px] w-40 h-20 bg-white flex justify-center items-center border shadow-sm"
-  >
-    <h1 class="text-xl">Content</h1>
-  </main>
-</template>
